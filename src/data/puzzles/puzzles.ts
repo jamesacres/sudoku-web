faf060ed09bec2338b456f45a3a69b9e41520ad5
James Acres <jamesacres@gmail.com>
2024-02-10 17:36:18 +0000

    update to sudoku from wikipedia

--- src/data/puzzles/puzzles.ts
+++ src/data/puzzles/puzzles.ts
@@ -2,17 +2,17 @@ import { puzzleTextToPuzzle } from '@/helpers/puzzleTextToPuzzle';
 import { Puzzle } from '@/types/puzzle';
 
 const puzzle1 = `
-123 456 789
-123 456 789
-123 456 789
+530 070 000
+600 195 000
+098 000 060
 
-123 456 789
-123 406 789
-123 456 789
+800 060 003
+400 803 001
+700 020 006
 
-123 456 789
-123 456 789
-123 456 789
+060 000 280
+000 419 005
+000 080 079
 `;
 
 const puzzles: { [puzzleId: number]: Puzzle } = {
