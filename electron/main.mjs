b3e012e77fad3aff20d26959a4a813517b601d7d
James Acres <jamesacres@gmail.com>
2024-05-28 22:05:14 +0100

    electron initial commit

---
+++ electron/main.mjs
@@ -0,0 +1,42 @@
+const { app, BrowserWindow } = await import('electron');
+const serve = await import('electron-serve');
+const path = await import('path');
+const __dirname = import.meta.dirname;
+
+const appServe = app.isPackaged
+  ? serve({
+      directory: path.join(__dirname, '../out'),
+    })
+  : null;
+
+const createWindow = () => {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js'),
+    },
+  });
+
+  if (app.isPackaged) {
+    appServe(win).then(() => {
+      win.loadURL('app://-');
+    });
+  } else {
+    win.loadURL('http://localhost:3000/puzzle/1');
+    win.webContents.openDevTools();
+    win.webContents.on('did-fail-load', (e, code, desc) => {
+      win.webContents.reloadIgnoringCache();
+    });
+  }
+};
+
+app.on('ready', () => {
+  createWindow();
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
