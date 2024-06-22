d8576faa60bf342ee005ce8cc0e03a7e73bf930b
James Acres <jamesacres@gmail.com>
2024-06-22 14:44:08 +0100

    Capacitor initial setup

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.bubblyclouds.sudoku',
+  appName: 'Sudoku',
+  webDir: 'out',
+};
+
+export default config;
