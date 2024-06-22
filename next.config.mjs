cacbb2f083bab7bd4e86eaae906ddae70382edd8
James Acres <jamesacres@gmail.com>
2024-06-22 15:16:25 +0100

    capacitor build scripts

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig =
-  process.env.IS_ELECTRON === 'true'
+  process.env.IS_ELECTRON === 'true' || process.env.IS_CAPACITOR === 'true'
     ? {
         output: 'export',
         images: {
