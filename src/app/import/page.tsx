50181fc6090d88a0dee70ec558238fc9e12ec6d1
James Acres <jamesacres@gmail.com>
2024-07-16 22:58:50 +0100

    add wasm sudoku solver

---
+++ src/app/import/page.tsx
@@ -0,0 +1,39 @@
+'use client';
+
+import Script from 'next/script';
+import { useCallback, useState } from 'react';
+
+export default function Home() {
+  const [solution, setSolution] = useState<string | undefined>(undefined);
+  const ready = useCallback(() => {
+    setSolution(
+      (window as any).Module.ccall(
+        'solve',
+        'string',
+        ['string'],
+        [
+          '.1...3.942....5...7....82...67......1..4....6.4..81..5....72.....3....8.......1.3',
+        ]
+      )
+    );
+  }, []);
+  return (
+    <>
+      {solution !== undefined && <div>{solution}</div>}
+      <Script
+        src="/solve.js"
+        onReady={() => {
+          if ((window as any).Module) {
+            if ((window as any).Module.onRuntimeInitialized) {
+              ready();
+            } else {
+              (window as any).Module.onRuntimeInitialized = function () {
+                ready();
+              };
+            }
+          }
+        }}
+      />
+    </>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,10 @@
 import Link from 'next/link';
 
 export default function Home() {
-  return <Link href="/puzzle/1">Puzzle 1</Link>;
+  return (
+    <>
+      <Link href="/puzzle/1">Puzzle 1</Link>
+      <Link href="/import">Import</Link>
+    </>
+  );
 }
