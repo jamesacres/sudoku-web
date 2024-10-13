ef932a5aead43b2542a018f0bc39efa7e6c0bddd
James Acres <jamesacres@gmail.com>
2024-10-13 17:02:36 +0100

    disable login when offline

--- src/components/HeaderUser/HeaderUser.tsx
+++ src/components/HeaderUser/HeaderUser.tsx
@@ -3,10 +3,12 @@
 import React from 'react';
 import { UserContext } from '../../providers/UserProvider';
 import { UserButton } from './UserButton';
+import { useOnline } from '@/hooks/online';
 
 const HeaderUser = () => {
   const { isLoggingIn, loginRedirect, logout, user } =
     React.useContext(UserContext) || {};
+  const { isOnline } = useOnline();
 
   const login = () => {
     if (loginRedirect) {
@@ -16,13 +18,15 @@ const HeaderUser = () => {
   return user && logout ? (
     <UserButton user={user} logout={logout} />
   ) : (
-    <button
-      disabled={isLoggingIn}
-      onClick={() => login()}
-      className={`${isLoggingIn ? 'cursor-wait' : ''} mr-4 mt-0 inline-block rounded border border-white px-4 py-2 text-sm leading-none text-white hover:border-transparent hover:bg-white hover:text-blue-500`}
-    >
-      Sign in
-    </button>
+    isOnline && (
+      <button
+        disabled={isLoggingIn}
+        onClick={() => login()}
+        className={`${isLoggingIn ? 'cursor-wait' : ''} mr-4 mt-0 inline-block rounded border border-white px-4 py-2 text-sm leading-none text-white hover:border-transparent hover:bg-white hover:text-blue-500`}
+      >
+        Sign in
+      </button>
+    )
   );
 };
 
