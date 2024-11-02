bf2e534d2a354ff5440cfb00958c3046543b6aae
James Acres <jamesacres@gmail.com>
2024-11-02 16:49:50 +0000

    improve timer display

--- src/components/TimerDisplay/TimerDisplay.tsx
+++ src/components/TimerDisplay/TimerDisplay.tsx
@@ -2,8 +2,8 @@ import { formatSeconds } from '@/helpers/formatSeconds';
 import { Watch } from 'react-feather';
 
 const TimerDisplay = ({ seconds }: { seconds: number }) => (
-  <p>
-    <Watch className="inline" /> {formatSeconds(seconds)}
+  <p className="mb-8 p-4 text-center font-mono">
+    <Watch className="m-auto inline" size={24} /> {formatSeconds(seconds)}
   </p>
 );
 
