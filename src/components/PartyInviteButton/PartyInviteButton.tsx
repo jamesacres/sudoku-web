658ef5861a0dcde16722d3a17500834956071652
James Acres <jamesacres@gmail.com>
2024-11-10 15:37:41 +0000

    correct copy link

--- src/components/PartyInviteButton/PartyInviteButton.tsx
+++ src/components/PartyInviteButton/PartyInviteButton.tsx
@@ -37,7 +37,7 @@ const PartyInviteButton = ({
     setIsLoading(false);
 
     if (latestInviteUrl) {
-      navigator.clipboard.writeText(inviteUrl);
+      navigator.clipboard.writeText(latestInviteUrl);
       setShowCopied(true);
       setTimeout(() => {
         setShowCopied(false);
