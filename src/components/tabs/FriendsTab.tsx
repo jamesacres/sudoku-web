3228c7aa88cc82fb627081a31e4abc049c8ab3e4
James Acres <jamesacres@gmail.com>
2025-06-09 23:29:11 +0100

    feat: show more puzzles per row

--- src/components/tabs/FriendsTab.tsx
+++ src/components/tabs/FriendsTab.tsx
@@ -73,7 +73,7 @@ export const FriendsTab = ({
                             )}
                           </button>
                           {userSessions[userId]?.sessions && (
-                            <ul className="grid grid-cols-2 gap-2 sm:grid-cols-3 sm:gap-4">
+                            <ul className="grid grid-cols-2 gap-2 sm:grid-cols-3 sm:gap-4 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6">
                               {userSessions[userId]?.sessions?.map(
                                 (userSession) => (
                                   <SessionRow
--- src/components/tabs/MyPuzzlesTab.tsx
+++ src/components/tabs/MyPuzzlesTab.tsx
@@ -19,7 +19,7 @@ export const MyPuzzlesTab = ({ sessions }: MyPuzzlesTabProps) => {
       {!!inProgress?.length && (
         <div className="mb-4">
           <h2 className="mb-2 text-2xl font-extrabold">In Progress</h2>
-          <ul className="grid grid-cols-2 gap-2 sm:grid-cols-3 sm:gap-4">
+          <ul className="grid grid-cols-2 gap-2 sm:grid-cols-3 sm:gap-4 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6">
             {inProgress?.map((session) => (
               <SessionRow
                 key={session.sessionId}
@@ -33,7 +33,7 @@ export const MyPuzzlesTab = ({ sessions }: MyPuzzlesTabProps) => {
       {!!completed?.length && (
         <div className="mb-4">
           <h2 className="mb-2 text-2xl font-extrabold">Completed</h2>
-          <ul className="grid grid-cols-2 gap-2 sm:grid-cols-3 sm:gap-4">
+          <ul className="grid grid-cols-2 gap-2 sm:grid-cols-3 sm:gap-4 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6">
             {completed?.map((session) => (
               <SessionRow
                 key={session.sessionId}
