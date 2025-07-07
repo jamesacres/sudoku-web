de7b431160c22344b0fb5499235f2a12bfedc368
James Acres <jamesacres@gmail.com>
2025-07-07 21:23:32 +0100

    chore: npm run build:capacitor

--- src/app/[...not_found]/page.tsx
+++ src/app/[...not_found]/page.tsx
@@ -2,4 +2,8 @@ import { notFound } from 'next/navigation';
 
 const NotFoundCatchAll = () => notFound();
 
+export const generateStaticParams = async () => {
+  return [{ not_found: ['not_found'] }];
+};
+
 export default NotFoundCatchAll;
