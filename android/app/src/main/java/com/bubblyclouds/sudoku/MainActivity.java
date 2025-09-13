12f8f2723cae4136deafaffd40b33371af3ab03e
James Acres <jamesacres@gmail.com>
2025-09-13 15:43:28 +0100

    fix: remove safe area plugin as fixed in webview 136

--- android/app/src/main/java/com/bubblyclouds/sudoku/MainActivity.java
+++ android/app/src/main/java/com/bubblyclouds/sudoku/MainActivity.java
@@ -5,11 +5,14 @@ import android.os.Bundle;
 import com.getcapacitor.BridgeActivity;
 
 import com.bubblyclouds.sudoku.plugins.SafeAreaPlugin.SafeAreaPlugin;
+import com.getcapacitor.Logger;
 
 public class MainActivity extends BridgeActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        registerPlugin(SafeAreaPlugin.class);
+        Logger.debug("MainActivity onCreate");
+        // No longer needed as Android System Webview 136 fixed it https://issues.chromium.org/issues/40699457
+        // registerPlugin(SafeAreaPlugin.class);
         super.onCreate(savedInstanceState);
     }
 }
