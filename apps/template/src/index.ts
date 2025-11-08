557e55aa8e1792b18aebd6c06d18cc6f928bd79f
James Acres <jamesacres@gmail.com>
2025-11-08 15:53:59 +0000

    remove duplocate playerColors.ts

--- apps/template/src/index.ts
+++ apps/template/src/index.ts
@@ -2,5 +2,4 @@
 export * from './components';
 // Note: Hooks are now imported from @sudoku-web/template package, not exported from app
 export * from './types';
-export * from './utils';
-// Note: Config has been moved to packages/template, import from @sudoku-web/template directly
+// Note: Utils have been moved to packages/template, import from @sudoku-web/template directly
